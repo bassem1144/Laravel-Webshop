fd60dfcd2bb8fcac28c1f17d6057bc64229c0fac
Bassem <bassem.ismail@student.rocva.nl>
2023-01-13 10:27:49 +0100

    added bootstrap
    products get shown directly from the database to the homepage

--- webshop-app/app/Http/Controllers/homepagecontroller.php
+++ webshop-app/app/Http/Controllers/homepagecontroller.php
@@ -2,33 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\products;
 use Illuminate\Http\Request;
 
 class homepagecontroller extends Controller
 {
-    public array $products = [
-        "Product1" => [
-            "name" => "Product1",
-            "price" => 100,
-            "description" => "This is a product",
-            "image" => "https://picsum.photos/200/150"
-        ],
-        "Product2" => [
-            "name" => "Product2",
-            "price" => 200,
-            "description" => "This is a product",
-            "image" => "https://picsum.photos/200/150"
-        ],
-        "Product3" => [
-            "name" => "Product3",
-            "price" => 300,
-            "description" => "This is a product",
-            "image" => "https://picsum.photos/200/150"
-        ],
-    ];
 
     public function index()
     {
-        return view('home', ['products' => $this->products]);
+        return view('home', ['products' => products::all()]);
     }
 }
--- webshop-app/resources/views/home.blade.php
+++ webshop-app/resources/views/home.blade.php
@@ -1,8 +1,33 @@
-<h1> home page </h1>
-<h2> products </h2>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <title>Home</title>
+</head>
+<body>
+    <h1> home page </h1>
+    <h2> products </h2>
 
-@foreach ($products as $product )
-    <h3> {{ $product['name'] }} </h3>
-    <p> {{ $product['description'] }} </p>
-    <p> {{ $product['price'] }} </p>
-@endforeach
+    <div class="card-group">
+
+    @foreach ($products as $product)
+        <div class="card">
+          <img class="card-img-top" src="https://picsum.photos/300/100" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">{{ $product['name'] }}</h5>
+            <p class="card-text">{{ $product['description'] }}</p>
+            <p class="card-text">&euro; {{ $product['price'] }}</p>
+            <p class="card-text"><small class="text-muted">Last updated: {{ $product['updated_at'] }}</small></p>
+          </div>
+        </div>
+    @endforeach
+
+      </div>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</body>
+</html>
+
+
