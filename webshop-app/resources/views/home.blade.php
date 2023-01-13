6f96b145af45ff46e912efec444ba0ebc5d11a7b
Bassem <bassem.ismail@student.rocva.nl>
2023-01-13 11:55:25 +0100

    home page style update

--- webshop-app/resources/views/home.blade.php
+++ webshop-app/resources/views/home.blade.php
@@ -1,33 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title>Home</title>
 </head>
+
 <body>
-    <h1> home page </h1>
-    <h2> products </h2>
-
-    <div class="card-group">
-
-    @foreach ($products as $product)
-        <div class="card">
-          <img class="card-img-top" src="https://picsum.photos/300/100" alt="Card image cap">
-          <div class="card-body">
-            <h5 class="card-title">{{ $product['name'] }}</h5>
-            <p class="card-text">{{ $product['description'] }}</p>
-            <p class="card-text">&euro; {{ $product['price'] }}</p>
-            <p class="card-text"><small class="text-muted">Last updated: {{ $product['updated_at'] }}</small></p>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <a class="nav-item nav-link active" href="#">Home</a>
+            <a class="nav-item nav-link" href="#">button 1</a>
+            <a class="nav-item nav-link" href="#">button 2</a>
+            <a class="nav-item nav-link" href="#">button 3</a>
+            
           </div>
         </div>
-    @endforeach
+      </nav>
 
-      </div>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <div class="card-deck row ">
+
+        @foreach ($products as $product)
+            <div class="card  m-3 col-2">
+                <img class="card-img-top" src="https://picsum.photos/200/150" alt="product image">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $product['name'] }}</h5>
+                    <p class="card-text">{{ $product['description'] }}</p>
+                </div>
+                <div class="card-footer">
+                    <small class="text-muted">{{ $product['updated_at'] }}</small>
+                </div>
+            </div>
+        @endforeach
+
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+    </script>
 </body>
+
 </html>
-
-
