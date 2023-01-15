ea0f3d108448eda5979a2dfa59877b1a7ed4b4de
Bassem <bassem.ismail@student.rocva.nl>
2023-01-15 20:28:17 +0100

    error handling for create form
    a message gets shown when the user leaves a field empty.
    previous input will stay

--- webshop-app/resources/views/create.blade.php
+++ webshop-app/resources/views/create.blade.php
@@ -2,19 +2,33 @@
 <html lang="en">
 
 <x-header></x-header>
-    <title>Create</title>
+<title>Create</title>
 
 <body>
     <h1>Create product</h1>
-    
+
     <form method="POST" action="/store">
         @csrf
+
         <label for="name">Name</label><br><br>
-        <input type="text" name="name" placeholder="Name"><br><br>
+        <input type="text" name="name" placeholder="Name" value="{{ old('name') }}"><br><br>
+        @error('name')
+            <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+
         <label for="description">Description</label><br><br>
-        <input type="text" name="description" placeholder="Description"><br><br>
+        <input type="text" name="description" placeholder="Description" value="{{ old('description') }}"><br><br>
+        @error('description')
+            <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+
         <label for="price">Price</label><br><br>
-        <input type="text" name="price" placeholder="Price"><br><br>
+        <input type="text" name="price" placeholder="Price" value="{{ old('price') }}"><br><br>
+        @error('price')
+            <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+
         <button type="submit">create</button>
 </body>
+
 </html>
