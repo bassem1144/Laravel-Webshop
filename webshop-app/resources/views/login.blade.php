fea7f285e62138428c91a7309dabf14debc3108a
Bassem <bassem.ismail@student.rocva.nl>
2023-01-17 14:39:31 +0100

    added login page

---
+++ webshop-app/resources/views/login.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<x-header></x-header>
+    <title>login</title>
+
+<body>
+    <h1>Login</h1>
+
+    <form action="/login" method="POST">
+        @csrf
+
+        <div class="form-group">
+            <label for="email">Email address</label>
+            <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp"
+                placeholder="Enter email">
+            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</body>
+</html>
