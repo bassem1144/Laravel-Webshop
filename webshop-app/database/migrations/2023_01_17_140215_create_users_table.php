ad846c38b6262db1fb48d41c979e04f939664a93
Bassem Ismail <bassem.ismail@hva.nl>
2026-01-01 01:52:53 +0200

    Fix users table migration with remember_token

--- webshop-app/database/migrations/2023_01_17_140215_create_users_table.php
+++ webshop-app/database/migrations/2023_01_17_140215_create_users_table.php
@@ -17,7 +17,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role')->default('customer');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
