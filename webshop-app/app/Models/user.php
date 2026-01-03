f2420e7f89e20608a54be67b156340eb0469270f
Bassem Ismail <bassem.ismail@hva.nl>
2026-01-03 16:28:13 +0200

    Add Order model import to User model

--- webshop-app/app/Models/user.php
+++ webshop-app/app/Models/user.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\Order;
 
 class User extends Authenticatable
 {
