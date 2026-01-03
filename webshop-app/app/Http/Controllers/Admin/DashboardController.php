869ad8a1bebc5e35315409880c6d1edfbd186311
Bassem Ismail <bassem.ismail@hva.nl>
2026-01-03 16:30:49 +0200

    Remove unused Request import from DashboardController

--- webshop-app/app/Http/Controllers/Admin/DashboardController.php
+++ webshop-app/app/Http/Controllers/Admin/DashboardController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Product;
-use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
